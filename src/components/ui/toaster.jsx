e6f2a98bdea9919df20f67836bbe4c0fa96e9ad1
Priyanshu <alokmishraa353@gmail.com>
2025-11-23 22:46:37 +0530

    Updated project

---
+++ src/components/ui/toaster.jsx
@@ -0,0 +1,24 @@
+import { useToast } from "@/hooks/use-toast";
+import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from "@/components/ui/toast";
+
+export function Toaster() {
+  const { toasts } = useToast();
+
+  return (
+    /* generic removed */
+      {toasts.map(function ({ id, title, description, action, ...props }) {
+        return (
+          <Toast key={id} {...props}>
+            <div className="grid gap-1">
+              {title && /* generic removed */{title}</ToastTitle>}
+              {description && /* generic removed */{description}</ToastDescription>}
+            </div>
+            {action}
+            <ToastClose />
+          </Toast>
+        );
+      })}
+      <ToastViewport />
+    </ToastProvider>
+  );
+}
